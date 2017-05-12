4065298fb289d166113d2b75eb32b0a69edf51e1
Naoise Golden <naoise.golden@gmail.com>
2017-05-12 13:37:04 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const app = require('express')();
+const request = require('request');
+const querystring = require('querystring');
+const get = require('lodash/get');
+
+const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY;
+const GOOGLE_PLACES_SERVICE = 'https://maps.googleapis.com/maps/api/place/autocomplete/json';
+const PORT = process.env.PORT || 8080;
+
+const firstPredictionSelector = (data) => get(data, 'predictions[0].description');
+
+const params = {
+  input,
+  key: GOOGLE_API_KEY,
+  location: '41.390205,2.154007', // Barcelona, Spain
+  radius: 10000, // 10km
+  types: 'address',
+  strictbounds: true
+};
+
+app.get('/', (req, res) => {
+  const input = req.query.q;
+
+  if(!input) {
+    res.send('ERROR: no input specified');
+  }
+
+  params['input'] = input;
+
+  const options = {
+    url: `${GOOGLE_PLACES_SERVICE}?${querystring.stringify(params)}`,
+    json: true
+  };
+
+  request(options, (error, response, body) => {
+    res.send(firstPredictionSelector(body));
+  });
+});
+
+app.listen(PORT, () => {
+  console.log('App is up');
+});
